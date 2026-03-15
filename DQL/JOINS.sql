960570d45ae085d4bf028e7fae92e142f9fb7570
Mayank Singh <mayanksingh0409@gmail.com>
2026-03-15 23:44:13 +0530

    Types of JOINS
    Updated the description of JOINS and added types.

--- DQL/JOINS.sql
+++ DQL/JOINS.sql
@@ -1,2 +1,12 @@
 # JOINS
-It is used to retrived the data from differnt tables simultaneously.
+It is used to retrived the data from the multiple table simultaneously.
+
+There are 5 types in JOIN  
+1. CARTESIAN JOIN / CROSS JOIN
+2. INNER JOIN / EQUI JOIN
+3. OUTER JOIN
+  1. LEFT OUTER JOIN
+  2. RIGHT OUTER JOIN
+  3. FULL OUTER JOIN
+4. NATURAL JOIN
+5. SELF JOIN
