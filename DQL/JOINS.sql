41110c149057a7577df44f3d89e21150cac01816
Mayank Singh <mayanksingh0409@gmail.com>
2026-03-15 23:52:49 +0530

    CROSS JOIN
    Expanded the explanation of JOIN types with syntax examples.

--- DQL/JOINS.sql
+++ DQL/JOINS.sql
@@ -2,11 +2,19 @@
 It is used to retrived the data from the multiple table simultaneously.
 
 There are 5 types in JOIN  
-1. CARTESIAN JOIN / CROSS JOIN
-2. INNER JOIN / EQUI JOIN
+1. CARTESIAN JOIN / CROSS JOIN - This statement is used to merge the records of table 1 with all the records of table 2.
+SYNTAX - 
+  i. ANSI - American National Standard Institute
+            SELECT COLUMN_NAME
+            FROM TABLE_NAME 1 CROSS JOIN TABLE_NAME 2;
+  ii. ORACLE - SELECT COLUMN_NAME
+               FROM TABLE_NAME 1, TABLE_NAME 2;
+
+2. INNER JOIN / EQUI JOIN - It is used to obtain the matching records.
+  
 3. OUTER JOIN
-  1. LEFT OUTER JOIN
-  2. RIGHT OUTER JOIN
-  3. FULL OUTER JOIN
+  i. LEFT OUTER JOIN
+  ii. RIGHT OUTER JOIN
+  iii. FULL OUTER JOIN
 4. NATURAL JOIN
 5. SELF JOIN
